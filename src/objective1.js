23872dfa00540ef9ca6c5e02b77f1a8a10011cbd
puneetbing <puneetvnit@gmail.com>
2018-12-20 17:19:02 +0530

    objective1 complete

--- src/objective1.js
+++ src/objective1.js
@@ -1,19 +1,46 @@
 module.exports = checkJira
 
 async function checkJira (context, config) {
-  const { pr_description, head } = context.payload.description
-  // console.log("~~~~~~>>>>>>>",context)
-  const collabCheck = pr_description.includes("Collaborators:")
-  const deployCheck = pr_description.includes("Deployment Type:")
-  const collabCheck = pr_description.includes("Collaborators: ")
-  const jiraCheck = pr_description.includes("Collaborators: ")
-  const state = wipPresent ? 'pending' : 'success'
+  const pr_description = context.payload.pull_request.body
+  const head = context.payload.pull_request.head
+  // console.log("~~~~~~>>>>>>>",pr_description)
+  const collabCheck = pr_description.match(/Collaborators:\s?(\w,?)+/g)
+  const deployCheck = pr_description.match(/Deployment Type:\s?(AppEngine| Engine|AppExtension|Extension)/g)
+  const jiraCheck = pr_description.match(/JIRA URL:\s?https:\/\/hiverhq\.atlassian\.net\/browse\/ENGG-\d+/)
+  const thingsToTest = pr_description.match(/Things to be tested:\s?.+/g)
+  // const jiraCheck = pr_description.includes("Collaborators: (AppEngine| Engine|AppExtension|Extension)")
+  let willMerge = true;
+
+  // console.log("~~~~~~>>>>>>>",collabCheck);
+  // console.log("~~~~~~>>>>>>>",deployCheck);
+  // console.log("~~~~~~>>>>>>>",jiraCheck);
+  // console.log("~~~~~~>>>>>>>",thingsToTest);
+  let bot_description = "";
+
+  if(!deployCheck) {
+    willMerge = false;
+    bot_description = "Deployment group not present " 
+  }
+  if(!collabCheck) {
+    willMerge = false;
+    bot_description = "Collaborators not present " 
+  }
+  if(!jiraCheck) {
+    willMerge = false;
+    bot_description = "Jira link not present" 
+  }
+  if(!thingsToTest) {
+    willMerge = false;
+    bot_description = "Things to test not present " 
+  }
+ // if()
+  const state = willMerge ? 'success' : 'pending'
 
   const status = {
     sha: head.sha,
     state,
     target_url: 'https://github.com/puneetbing',
-    description: 'WIP present check',
+    description: bot_description,
     context: 'Pull Request Tests'
   }
 
